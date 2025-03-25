ff633501d4be687c8f45333eb07a8703c8bd8f10
yusuf <yusuf@172.20.144.132>
2025-03-25 08:59:24 +0300

    camera improvement, cannot go out of map

--- src/main/Camera.java
+++ src/main/Camera.java
@@ -17,9 +17,24 @@ public class Camera {
 	
 	public void followEntity(Entity entity) {
 		
-		cameraX = entity.worldX - (gameP.screenWidth / 2) + (gameP.tileSize / 2);;
-		cameraY = entity.worldY - (gameP.screenHeight / 2) + (gameP.tileSize / 2);;
+		cameraX = entity.worldX - (gameP.screenWidth / 2) + (gameP.tileSize / 2);
+		cameraY = entity.worldY - (gameP.screenHeight / 2) + (gameP.tileSize / 2);
 		
+		if (cameraX < 0) {
+			cameraX = 0;
+		}
+		
+		else if (cameraX > gameP.tileSize * gameP.tileM.mapTiles[0].length - gameP.screenWidth) {
+			cameraX = gameP.tileSize * gameP.tileM.mapTiles[0].length - gameP.screenWidth;
+		}
+		
+		if (cameraY < 0) {
+			cameraY = 0;
+		}
+		
+		else if (cameraY > gameP.tileSize * gameP.tileM.mapTiles.length - gameP.screenHeight) {
+			cameraY = gameP.tileSize * gameP.tileM.mapTiles.length - gameP.screenHeight;
+		}
 	}
 
 }
